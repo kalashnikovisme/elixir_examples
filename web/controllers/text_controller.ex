b8a473e603de0fe3dacd38ffabcc1dae48899aee
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2016-10-01 04:31:04 +0400

    Working with many letter

--- web/controllers/text_controller.ex
+++ web/controllers/text_controller.ex
@@ -1,64 +1,13 @@
+require IEx
 defmodule PhoenixFirstApp.TextController do
   use PhoenixFirstApp.Web, :controller
 
   alias PhoenixFirstApp.Text
   import AlchemicAvatar
 
-  def index(conn, _params) do
-    texts = Repo.all(Text)
-    render(conn, "index.html", texts: texts)
-  end
-
-  def new(conn, _params) do
-    changeset = Text.changeset(%Text{})
-    render(conn, "new.html", changeset: changeset)
-  end
-
-  def create(conn, %{"text" => text_params}) do
-    changeset = Text.changeset(%Text{}, text_params)
-
-    case Repo.insert(changeset) do
-      {:ok, _text} ->
-        conn
-        |> put_flash(:info, "Text created successfully.")
-      {:error, changeset} ->
-        render(conn, "new.html", changeset: changeset)
-    end
-  end
-
   def show(conn, %{"text" => text}) do
-    image = AlchemicAvatar.generate text, 200
-    render(conn, "show.html", image: image)
-  end
-
-  def edit(conn, %{"id" => id}) do
-    text = Repo.get!(Text, id)
-    changeset = Text.changeset(text)
-    render(conn, "edit.html", text: text, changeset: changeset)
-  end
-
-  def update(conn, %{"id" => id, "text" => text_params}) do
-    text = Repo.get!(Text, id)
-    changeset = Text.changeset(text, text_params)
-
-    case Repo.update(changeset) do
-      {:ok, text} ->
-        conn
-        |> put_flash(:info, "Text updated successfully.")
-        |> redirect(to: text_path(conn, :show, text))
-      {:error, changeset} ->
-        render(conn, "edit.html", text: text, changeset: changeset)
-    end
-  end
-
-  def delete(conn, %{"id" => id}) do
-    text = Repo.get!(Text, id)
-
-    # Here we use delete! (with a bang) because we expect
-    # it to always work (and if it does not, it will raise).
-    Repo.delete!(text)
-
-    conn
-    |> put_flash(:info, "Text deleted successfully.")
+    chars = Enum.drop String.split(text, ""), -1
+    images = Enum.map chars, fn(x) -> AlchemicAvatar.generate x, 200 end
+    render(conn, "show.html", images: images)
   end
 end
